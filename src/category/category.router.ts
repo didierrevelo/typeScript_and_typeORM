fbf84737c91f62284f41c0ce481d93faa13c7808
didierrevelo <didierrevelo@gmail.com>
2023-02-11 19:27:13 -0500

    created category created controller, dto, services and routes

---
+++ src/category/category.router.ts
@@ -0,0 +1,26 @@
+import { BaseRouter } from '../shared/router'
+import { CategoryController } from './controller/category.controller'
+
+export class CategoryRouter extends BaseRouter<CategoryController> {
+  constructor () {
+    super(CategoryController)
+  }
+
+  routes (): void {
+    this.router.get('/categories', (req, res) =>
+      this.controller.getCategories(req, res)
+    )
+    this.router.get('/category/:id', (req, res) =>
+      this.controller.getCategoryById(req, res)
+    )
+    this.router.post('/createCategory', (req, res) =>
+      this.controller.createCategory(req, res)
+    )
+    this.router.put('/updateCategory/:id', (req, res) =>
+      this.controller.updateCategory(req, res)
+    )
+    this.router.delete('/deleteCategory/:id', (req, res) =>
+      this.controller.deleteCategory(req, res)
+    )
+  }
+}
